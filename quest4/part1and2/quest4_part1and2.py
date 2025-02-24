940db3f96dba22ecfc1261f226b2525c003c7227
dthiele <daniel.thiele@dguv.de>
2025-02-24 13:20:30 +0100

    add quest 4

---
+++ quest4/part1and2/quest4_part1and2.py
@@ -0,0 +1,9 @@
+import pathlib
+
+with open(pathlib.Path(__file__).parent / 'example.txt') as inf:
+    nails = [int(n.strip()) for n in inf.readlines()]
+
+height = min(nails)
+solution = sum(n - height for n in nails)
+
+print(solution)
---
+++ quest4/part3/quest4_part3.py
@@ -0,0 +1,10 @@
+import pathlib
+from statistics import median_low
+
+with open(pathlib.Path(__file__).parent / 'example.txt') as inf:
+    nails = [int(n.strip()) for n in inf.readlines()]
+
+height = median_low(nails)
+solution = sum(abs(n - height) for n in nails)
+
+print(solution)
