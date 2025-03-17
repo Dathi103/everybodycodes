cdabf57fd86e6a8cce20504da5e4d7147de6ee48
dthiele <daniel.thiele@dguv.de>
2025-03-17 12:54:16 +0100

    format quest 8 part 1

--- quest8/part1/part1.py
+++ quest8/part1/part1.py
@@ -2,7 +2,7 @@ import pathlib
 
 
 def main():
-    with open(pathlib.Path(__file__).parent / 'notes.txt') as inf:
+    with open(pathlib.Path(__file__).parent / "notes.txt") as inf:
         n_blocks = int(inf.read().strip())
 
     blocks_needed = 1
@@ -13,13 +13,14 @@ def main():
             width += 1
         else:
             width += 2
-        
+
         n_blocks -= blocks_needed
         blocks_needed += 2
 
     result = (width + 2) * (blocks_needed - n_blocks)
 
     print(result)
+
 
 if __name__ == "__main__":
     main()
