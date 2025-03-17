4b3cb4e27e82f157b883cdb708acfc1cdc425ef4
dthiele <daniel.thiele@dguv.de>
2025-03-17 12:54:46 +0100

    correct quest 8 part 2

--- quest8/part2/part2.py
+++ quest8/part2/part2.py
@@ -1,57 +1,37 @@
 import pathlib
 
 
+def build_tower(thickness, old_tower) -> tuple[dict[int, int], int]:
+    new_tower = {}
+    needed = 0
+    width = old_tower.get(0, -1) + 2
+    for i in range(thickness):
+        needed += width - old_tower.get(i, 0)
+        new_tower[i] = width
+    for i in range(len(old_tower)):
+        needed += old_tower[i] - old_tower.get(i + thickness, 0)
+        new_tower[i + thickness] = old_tower[i]
+
+    return new_tower, needed
+
+
 def main():
-    with open(pathlib.Path(__file__).parent / 'notes.txt') as inf:
+    with open(pathlib.Path(__file__).parent / "notes.txt") as inf:
         n_priests = int(inf.read().strip())
 
-    blocks_needed = 1
-    layer_widths = []
+    blocks_needed = 0
+    tower = {}
     thickness = 0
     n_acolytes = 5
     n_blocks = 50
 
-
-
     while n_blocks > blocks_needed:
         n_blocks -= blocks_needed
-        
-        if not layer_widths:
-            layer_widths += [1]
-            thickness = 1
-        
-        thickness = (thickness * n_priests) % n_acolytes
+        thickness = (thickness * n_priests) % n_acolytes or 1
+        tower, blocks_needed = build_tower(thickness, tower)
 
-        blocks_needed = 0
-        new_layer_widths = []
+    print((blocks_needed - n_blocks) * tower[0])
 
-        for i in range(thickness):
-            try:
-                old_thickness = layer_widths[i]
-            except IndexError:
-                old_thickness = 0
-
-            blocks_needed += thickness - old_thickness
-            new_layer_widths.append(thickness)
-
-        new_layer_widths.extend(layer_widths)
-
-        for i, w in enumerate(new_layer_widths[thickness:], start=thickness):
-            try:
-                old_thickness = layer_widths[i]
-            except IndexError:
-                old_thickness = 0
-            blocks_needed += w - old_thickness
-
-        width = layer_widths[0]
-        layer_widths = new_layer_widths
-
-
-        
-
-    result = (width + 2) * (blocks_needed - n_blocks)
-
-    print(result)
 
 if __name__ == "__main__":
     main()
